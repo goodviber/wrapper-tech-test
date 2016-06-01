fd0887c182ac4f2cd241e8730b1b8688334fcfc4
maxmills <8balldigitalsolutions@gmail.com>
2016-06-01 11:50:53 +0100

    wrapped

---
+++ spec/wrapper_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+require_relative '../wrapper'
+
+describe Wrapper do
+  it 'works with spaces' do
+    word_wrap = Wrapper.new
+    wrapped_string = word_wrap.wrap('hello there', 5)
+    expect(wrapped_string).to eq "hello\nthere\n"
+  end
+
+  it 'works with no spaces' do
+    word_wrap = Wrapper.new
+    wrapped_string = word_wrap.wrap('hellothere', 5)
+    expect(wrapped_string).to eq "hello\nthere\n"
+  end
+
+  it 'works with hyphens' do
+    word_wrap = Wrapper.new
+    wrapped_string = word_wrap.wrap('hello-there', 5)
+    expect(wrapped_string).to eq "hello\nthere\n"
+  end
+
+  it 'fails with stars' do
+    word_wrap = Wrapper.new
+    wrapped_string = word_wrap.wrap('hello*there', 5)
+    expect(wrapped_string).not_to eq "hello\nthere\n"
+  end
+end
